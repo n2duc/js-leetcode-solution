4307bd4038085eb8fb3831a922d3e536d4ec14d6
Nguyen Duc <97241208+n2duc@users.noreply.github.com>
2022-10-26 10:09:05 +0700

    update

---
+++ largestNumber.js
@@ -0,0 +1,18 @@
+const largestNumber = (nums) => {
+    let sorted = nums.sort((a, b) => {
+        const ab = a.toString() + b.toString()
+        console.log(ab)
+        const ba = b.toString() + a.toString()
+        console.log(ba)
+        console.log(ba - ab)
+        return ba - ab
+    })
+    const joined = sorted.join('')
+    if(parseInt(joined) == 0) {
+        return '0'
+    } else {
+        return joined
+    }
+}
+let exp = [3, 9, 12, 5, 23]
+console.log(largestNumber(exp))
